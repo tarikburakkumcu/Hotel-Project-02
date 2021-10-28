e78ccb378426af693f88f8ef4baf3692ba953554
ferudungogus <86746223+ferudungogus@users.noreply.github.com>
2021-10-28 12:04:26 +0300

    v1 deneme

---
+++ src/test/java/test/smokeTests/uS_0003/TC_0001.java
@@ -0,0 +1,10 @@
+package test.smokeTests.uS_0003;
+
+import org.testng.annotations.Test;
+
+public class TC_0001 {
+    @Test
+    public void test(){
+        System.out.println("deneme");
+    }
+}

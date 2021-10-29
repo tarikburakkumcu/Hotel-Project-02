8e0e25a7530e414fd44829aff741425cb7005338
Oguzhan <akkafaoguz@gmail.com>
2021-10-29 10:43:12 +0300

    deneme

--- src/test/java/test/smokeTests/uS_0001/Tc_0002.java
+++ src/test/java/test/smokeTests/uS_0001/Tc_0002.java
@@ -20,21 +20,21 @@ public class Tc_0002 {
     public void tC_0002 (){
 
         reusableMethods.login();
-//        SoftAssert softAssert = new SoftAssert();
-//        mainPage.homeLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Home"),"Test for navigation of 'Home Button' is failed!");
-//        mainPage.roomsLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Rooms"),"Test for navigation of 'Rooms Button' is failed!");
-//        mainPage.restaurantLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Restaurant"),"Test for navigation of 'Restaurant Button' is failed!");
-//        mainPage.aboutLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("About"),"Test for navigation of 'About Button' is failed!");
-//        mainPage.blogLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Blog"),"Test for navigation of 'Blog Button' is failed!");
-//        mainPage.contactLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Contact"),"Test for navigation of 'Contact Button' is failed!");
-//        mainPage.loginLink.click();
-//        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Log in"),"Test for navigation of 'Log in Button' is failed!");
-//        softAssert.assertAll();
+        SoftAssert softAssert = new SoftAssert();
+        mainPage.homeLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Home"),"Test for navigation of 'Home Button' is failed!");
+        mainPage.roomsLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Rooms"),"Test for navigation of 'Rooms Button' is failed!");
+        mainPage.restaurantLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Restaurant"),"Test for navigation of 'Restaurant Button' is failed!");
+        mainPage.aboutLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("About"),"Test for navigation of 'About Button' is failed!");
+        mainPage.blogLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Blog"),"Test for navigation of 'Blog Button' is failed!");
+        mainPage.contactLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Contact"),"Test for navigation of 'Contact Button' is failed!");
+        mainPage.loginLink.click();
+        softAssert.assertTrue(Driver.getDriver().getTitle().contains("Log in"),"Test for navigation of 'Log in Button' is failed!");
+        softAssert.assertAll();
     }
 }

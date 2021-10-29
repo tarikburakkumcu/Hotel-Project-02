4bc3bbc4861d2c6f7cbc2a4fb53e77dfeff187a7
Samet <ismetkara229@gmail.com>
2021-10-29 10:36:12 +0300

    denemeDegisiklik

--- src/test/java/test/smokeTests/uS_0008/TC_0001.java
+++ src/test/java/test/smokeTests/uS_0008/TC_0001.java
@@ -3,5 +3,7 @@ package test.smokeTests.uS_0008;
 public class TC_0001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Degisiklik yapildi");
     }
+
 }

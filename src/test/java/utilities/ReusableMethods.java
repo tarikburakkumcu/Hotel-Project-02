a1114cb20f2c60802c8b2ec050ba4e3630dd6b26
ferudungogus <86746223+ferudungogus@users.noreply.github.com>
2021-11-02 20:03:39 +0300

    ferudun03

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,16 +1,25 @@
 package utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import pages.DefaultPage;
 import pages.LoginPage;
 import pages.MainPage;
+import pages.RegisterPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ReusableMethods {
 
     MainPage mainPage = new MainPage();
     LoginPage loginPage = new LoginPage();
     DefaultPage defaultPage = new DefaultPage();
-
+    RegisterPage registerPage=new RegisterPage();
     public void login() {
 
         goToUrl();
@@ -41,4 +50,19 @@ public class ReusableMethods {
         defaultPage.hotelManagementLinki.click();
         defaultPage.hotelRoomsLinki.click();
     }
+
+    public static String getScreenshot(String name) throws IOException {
+        // naming the screenshot with the current date to avoid duplication
+        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        // TakesScreenshot is an interface of selenium that takes the screenshot
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        // full path to the screenshot location
+        String target = System.getProperty("user.dir") + "/raporlar/Screenshots/" + name + date + ".png";
+        File finalDestination = new File(target);
+        // save the screenshot to the path given
+        FileUtils.copyFile(source, finalDestination);
+        return target;
+    }
+
 }
